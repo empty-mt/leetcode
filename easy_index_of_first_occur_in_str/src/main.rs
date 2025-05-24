fdc5fe9952f65f228a9a3a8f67cf8c998a75c243
empty_win <s0568465@htw-berlin.de>
2025-05-25 00:40:06 +0200

    rm comment

--- easy_index_of_first_occur_in_str/src/main.rs
+++ easy_index_of_first_occur_in_str/src/main.rs
@@ -1,4 +1,4 @@
-// struct Solution;
+struct Solution;
 impl Solution {
     pub fn str_str(haystack: String, needle: String) -> i32 {
         let result: Option<usize> = haystack.find(needle.as_str());
@@ -8,7 +8,7 @@ impl Solution {
         }
     }
 }
-/*
+
 // testing
 fn main() {
     assert_eq!(Solution::str_str("sadbutsad".to_string(), "sad".to_string()), 0);
@@ -17,4 +17,3 @@ fn main() {
     assert_eq!(Solution::str_str("banana".to_string(), "na".to_string()), 2);
     assert_eq!(Solution::str_str("jashnjashiqewjashjash".to_string(), "jashi".to_string()), 5);
 }
-*/
